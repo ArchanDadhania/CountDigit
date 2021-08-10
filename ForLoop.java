f4eead958c7b66d3cf7d3aee6e6e5e3d7200020a
ArchanDadhania <59695959+ArchanDadhania@users.noreply.github.com>
2021-08-10 18:21:45 +0530

    Create ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input Your Number:");
+        int num = sc.nextInt();
+
+        for (;num!=0;num/=10)
+        {
+            ++count;
+        }
+        System.out.println("Print number of digit:" +count);
+    }
+}
